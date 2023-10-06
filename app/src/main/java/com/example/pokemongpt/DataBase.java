95ad73e05e77e477bfd882a7f3eb3de23134f2a0
Chloe-Baillivet <chloe.baillivet@cpe.fr>
2023-10-06 11:21:38 +0200

    Fix nav bar

--- app/src/main/java/com/example/pokemongpt/DataBase.java
+++ app/src/main/java/com/example/pokemongpt/DataBase.java
@@ -1,3 +1,5 @@
+package com.example.pokemongpt;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,7 +10,7 @@ public class DataBase extends SQLiteOpenHelper {
     private static final int  version =1;
     private static final String DATABASE_NAME = "pokemomGPT.db";
 
-    private static final String DATABASE_CREATE = "create table pokedex (number integer primary key,name text not null;";
+    private static final String DATABASE_CREATE = "create table pokedex (number integer primary key,name text not null,type1 text not null,type2 text,height integer not null,weight integer not null,;";
     public DataBase(Context context){
         super(context, DATABASE_NAME, null, version);
     }
